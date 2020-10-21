5cedb8f82f12f5adce505bbccfdeb437c6057d5b
François LaBerge <fgrclaberge@gmail.com>
2020-10-20 22:36:25 -0400

    Added multiple term queries

--- ir/p2/query.py
+++ ir/p2/query.py
@@ -1,11 +1,14 @@
 import pickle
 import argparse
+import sys
+import json
 
-def query_term(term, index_file):
+def query_term(terms, index_file, output_file):
     index = load_index(index_file)
-    result = index[term] if term in index else []
-    print(result)
-    print(index)
+    result = {}
+    for term in terms:
+        result[term] = index[term] if term in index else []
+    print(json.dumps(result, indent=3), file=output_file)
 
 def load_index(index_file):
     with open(index_file, 'rb') as f:
@@ -14,7 +17,12 @@ def load_index(index_file):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Query a dictionnart term')
     parser.add_argument('-i', '--index', help="The index to look into", required=True)
-    parser.add_argument('-t', '--term', help="The term to lookup", metavar='term', required=True)
+    parser.add_argument('-t', '--terms', help="The terms to lookup", metavar='term', required=True, nargs='+')
+    parser.add_argument('-o', '--output', help="The path for the ouput file", default=sys.stdout, required=False)
 
     args = parser.parse_args()
-    query_term(args.term, args.index)
+
+    if type(args.output) is str:
+        args.output = open(args.output, 'w', encoding='utf-8')
+
+    query_term(args.terms, args.index, args.output)
