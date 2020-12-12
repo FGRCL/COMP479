2bdbe8bbd6adf0ad94a6fcda422879b48e5895cc
François LaBerge <fgrclaberge@gmail.com>
2020-12-11 22:49:56 -0500

    Added results files and readme

--- ir/p4/query.py
+++ ir/p4/query.py
@@ -18,11 +18,11 @@ def query(index_file: str, query: str, ranking_algorithm_choice: RankingAlgorith
     ranked_documents = ranking.rank(query_tokens, inverted_index)
 
     for i, ranked_document in enumerate(ranked_documents):
-        print(f'rank: {i+1} \tscore: {ranked_document[0].__round__(4)}\tdocument:{ranked_document[1]}')
+        print(f'rank: {i+1} \tscore: {ranked_document[0].__round__(10)}\tdocument:{ranked_document[1]}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Query a dictionary term')
-    parser.add_argument('--index', '-i', help="Path to the inverted index to use", type=str, default="index/index_big.pickle")
+    parser.add_argument('--index', '-i', help="Path to the inverted index to use", type=str, default="index/index.pickle")
     parser.add_argument('--query', '-q', help="the use query", type=str, required=True)
     parser.add_argument('--rankingalgorithm', '-ra', help="The ranking algorithm to use", choices=RankingAlgorithmChoice, type=RankingAlgorithmChoice.from_string, default=RankingAlgorithmChoice.tfidf)
     parser.add_argument('--output', '-o', help="The output file", type=str, required=False)
